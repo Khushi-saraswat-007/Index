1c499c85c88a91e6ecca2f8e528444e75c605502
Khushi Saraswat <khushisaraswat2007@gmail.com>
2026-08-11 21:01:47 +0530

    Added dark and light mode

---
+++ script.js
@@ -0,0 +1,33 @@
+// // // // //let data = [1,2,3,4,5,6,7,8,9];
+// // // // //for(let i=0 ;i < data.length;i++){
+// // // //     console.log(data[i]**2);
+// // // // //}
+// // // // FUNCTIONS
+// // // // function add(a,b){
+// // // //     console.log(a+b);
+// // // // }
+// // // // add(10,20);
+// // // var fn = () => {  #arrow fnction
+// // //     console.log("Called");
+// // // }
+// // // var add = (a,b)=>{
+// // //     console.log(a+b);
+// // // }
+// // // add(1,2);
+
+// // const product = function(x,y){  #expression function
+// //     console.log(x*y);
+// // }
+// // product(10,20);
+
+// var displayarr =(arr)=>{
+//     for(let i = 0; i<arr.length;i++){
+//     console.log(arr[i]);
+//     }
+// }
+
+// displayarr([1,2,3,4,5,6,7,8,9]);
+
+
+//DOM
+
